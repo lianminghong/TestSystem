60b9fe4e075c42ede49fc35d251a9d17e026d84b
lianminghong <lianminghong@vtron.com>
2017-12-05 11:11:15 +0800

    测试提交代码

---
+++ src/time/millisecondsToHour.java
@@ -0,0 +1,17 @@
+package time;
+
+import java.sql.Date;
+
+public class millisecondsToHour {
+
+	public millisecondsToHour(){
+		Date date = new Date(13716710);
+//		SimpleDateFormat formatter =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+		System.out.println(date);
+	}
+	
+	public static void main(String[] args) {
+		new millisecondsToHour();
+	}
+
+}
